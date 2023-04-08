2bac5af407a10a1018c397bc42dd9de24dfc2c3d
Ercan Ku <kuercan@proton.me>
2023-04-08 13:31:24 +0200

    chore: initial commit for base Spring Boot application

---
+++ src/test/java/app/ercan/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ercan.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
